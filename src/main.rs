787d991d7f94186341699a6291a7b88c866383c0
VV-WIN <51390159+VV-WIN@users.noreply.github.com>
2024-01-11 23:01:34 -0800

    Setup pong bevy project

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_ball, spawn_camera))
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty()
+            .insert(Camera2dBundle::default());
+}
+
+fn spawn_ball(mut commands: Commands) {
+    println!("Spawning ball...");
+    commands.spawn_empty();
+}
