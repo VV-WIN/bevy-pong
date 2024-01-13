e6881e3d3e3aed271944f47f5d55493151566362
VV-WIN <51390159+VV-WIN@users.noreply.github.com>
2024-01-13 04:20:10 -0800

    Add basic collision to test ball bounching off a paddle

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
+use bevy::sprite::collide_aabb::{collide, Collision};
 
 const BALL_SIZE: f32 = 5.;
 const BALL_SPEED: f32 = 5.;
-const PADDLE_SPEED: f32 = 1.;
-const PADDLE_HEIGHT: f32 = 10.;
-const PADDLE_WIDTH: f32 = 50.;
+const PADDLE_SPEED: f32 = 1.; // Will come in handy when we start to move the paddles
+const PADDLE_WIDTH: f32 = 10.;
+const PADDLE_HEIGHT: f32 = 50.;
 
 #[derive(Component)]
 struct Ball;
@@ -19,8 +20,6 @@ struct Shape(Vec2);
 #[derive(Component)]
 struct Velocity(Vec2);
 
-#[derive(Component)]
-struct Paddle;
 
 #[derive(Bundle)]
 struct BallBundle {
@@ -29,11 +28,6 @@ struct BallBundle {
     shape: Shape,
     velocity: Velocity
 }
- #[derive(Bundle)]
- struct PaddleBundle {
-     paddle: Paddle,
-     position: Position
- }
 
 impl BallBundle {
     fn new(x: f32, y: f32) -> Self {
@@ -46,25 +40,73 @@ impl BallBundle {
     }
 }
 
+#[derive(Component)]
+struct Paddle;
+
+#[derive(Bundle)]
+struct PaddleBundle {
+    paddle: Paddle,
+    shape: Shape,
+    position: Position,
+    velocity: Velocity,
+}
+
 impl PaddleBundle {
     fn new(x: f32, y: f32) -> Self {
         Self {
             paddle: Paddle,
-            position: Position(Vec2::new(x, y))
+            shape: Shape(Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
+            position: Position(Vec2::new(x, y)),
+            velocity: Velocity(Vec2::new(0., 0.)),
         }
     }
 }
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (
-            spawn_ball,
-            spawn_camera,
-            spawn_paddles
+        .add_systems(Startup, (spawn_ball, spawn_camera, spawn_paddles))
+        .add_systems(Update, (
+            move_ball,
+            project_positions.after(move_ball),
+            handle_collisions.after(move_ball),
         ))
-        .add_systems(Update, move_ball)
         .run();
+}
+
+fn handle_collisions(
+    mut ball: Query<(&mut Velocity, &Position, &Shape), With<Ball>>,
+    // We can collide with anything else that has a shape and position that is
+    // not itself a ball
+    other_things: Query<(&Position, &Shape), Without<Ball>>,
+) {
+    if let Ok((mut ball_velocity, ball_position, ball_shape)) = ball.get_single_mut() {
+        for (position, shape) in &other_things {
+            if let Some(collision) = collide(
+                ball_position.0.extend(0.), // position_a (Vec3)
+                ball_shape.0,               // size_a (Vec2)
+                position.0.extend(0.),      // position_b (Vec3)
+                shape.0,                    // size_b (Vec2)
+            ) {
+                match collision {
+                    Collision::Left => {
+                        ball_velocity.0.x *= -1.;
+                    }
+                    Collision::Right => {
+                        ball_velocity.0.x *= -1.;
+                    }
+                    Collision::Top => {
+                        ball_velocity.0.y *= -1.;
+                    }
+                    Collision::Bottom => {
+                        ball_velocity.0.y *= -1.;
+                    }
+                    Collision::Inside => {
+                        // Do nothing
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn project_positions(mut ball: Query<(&mut Transform, &Position)>) {
@@ -85,14 +127,16 @@ fn spawn_ball(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     println!("Spawning ball...");
-    let mesh = Mesh::from(shape::Circle::new(5.));
+
+    let mesh = Mesh::from(shape::Circle::new(BALL_SIZE));
     let material = ColorMaterial::from(Color::rgb(1., 0., 0.));
 
+    // Now our mesh shape is derived from the `Shape` we made as a new component
     let mesh_handle = meshes.add(mesh);
     let material_handle = materials.add(material);
 
     commands.spawn((
-        BallBundle::new(1., 1.),
+        BallBundle::new(1., 0.),
         MaterialMesh2dBundle {
             mesh: mesh_handle.into(),
             material: material_handle,
@@ -108,14 +152,18 @@ fn spawn_paddles(
 ) {
     println!("Spawning paddles...");
 
-    let mesh = Mesh::from(shape::Quad::new(Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)));
+    let mesh = Mesh::from(shape::Quad::new(Vec2::new(
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT,
+    )));
+
     let material = ColorMaterial::from(Color::rgb(0., 1., 0.));
 
     let mesh_handle = meshes.add(mesh);
     let material_handle = materials.add(material);
 
     commands.spawn((
-        PaddleBundle::new(20., -25.0),
+        PaddleBundle::new(20., -25.),
         MaterialMesh2dBundle {
             mesh: mesh_handle.into(),
             material: material_handle,
